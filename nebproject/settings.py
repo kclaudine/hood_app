309b0b0edb3a558f12195293722d36b8eaf76297
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-02-02 13:53:58 +0200

    add changes

--- nebproject/settings.py
+++ nebproject/settings.py
@@ -57,7 +57,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '%5ppk=bd(7tk$gydj(y$6mr=63^e0uxp@tnc1=+@q^9)y5gz^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
 
@@ -115,16 +115,16 @@ WSGI_APPLICATION = 'nebproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nhood',
-        'USER': 'wecode',
-        'PASSWORD':'coco',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nhood',
+#         'USER': 'wecode',
+#         'PASSWORD':'coco',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
