4599dbbe2211af8bfd54e0631bb0e13f1bf4c8b0
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-02-02 13:55:03 +0200

    add changes

--- nebproject/settings.py
+++ nebproject/settings.py
@@ -172,8 +172,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # deployment
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 # django_heroku.settings(locals())
