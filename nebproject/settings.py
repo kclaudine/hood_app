0758cd72d3fdc0b62c7f246971d0af6b01544406
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-02-02 13:55:18 +0200

    add changes

--- nebproject/settings.py
+++ nebproject/settings.py
@@ -176,7 +176,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 
 LOGIN_REDIRECT_URL = '/'
